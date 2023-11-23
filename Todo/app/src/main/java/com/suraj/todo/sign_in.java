6e5f0fefbbe0d2e065d06f82fb14a4a8c30cb56f
Suraj Meharwade <surajmeharwade@Surajs-MacBook-Pro.local>
2023-11-23 16:08:35 +0530

    Signup screen on todo app

---
+++ Todo/app/src/main/java/com/suraj/todo/sign_in.java
@@ -0,0 +1,16 @@
+package com.suraj.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
