5663ea1b8029d90f60d9341d903a1661f1b5e620
Suraj Meharwade <surajmeharwade@Surajs-MacBook-Pro.local>
2023-11-25 14:34:10 +0530

    Login and Signup with email is working

--- Todo/app/src/main/java/com/suraj/todo/sign_up.java
+++ Todo/app/src/main/java/com/suraj/todo/sign_up.java
@@ -6,62 +6,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
+import com.google.firebase.auth.FirebaseUser;
 import com.suraj.todo.databinding.ActivitySignInBinding;
+import com.suraj.todo.databinding.ActivitySignUpBinding;
 
-public class sign_in extends AppCompatActivity {
-    ActivitySignInBinding binding;
-    FirebaseAuth auth;
+public class sign_up extends AppCompatActivity {
+    ActivitySignUpBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         auth = FirebaseAuth.getInstance();
-        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(sign_in.this,sign_up.class));
-            }
-        });
 
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email,password;
+                startActivity(new Intent(sign_up.this,sign_in.class));
+                finish();
+            }
+        });
+        binding.signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password, confirmPassword;
                 email = String.valueOf(binding.emailINP.getText());
                 password = String.valueOf(binding.passwordINP.getText());
+                confirmPassword = String.valueOf(binding.confirmPasswordINP.getText());
                 if (TextUtils.isEmpty(email)) {
                     binding.emailINP.setError("Email cannot be empty");
                 }
                 if (TextUtils.isEmpty(password)) {
                     binding.passwordINP.setError("Password cannot be empty");
                 }
-                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                if (TextUtils.isEmpty(confirmPassword)) {
+                    binding.confirmPasswordINP.setError("Password cannot be empty");
+                }
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
-                            startActivity(new Intent(sign_in.this, MainActivity.class));
+                            Toast.makeText(sign_up.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(sign_up.this, MainActivity.class));
                             finish();
                         }
                         else {
-                            Log.d("Login Exception",task.getException().toString());
-                            if (task.getException().toString().contains("The supplied")){
-                                binding.passwordINP.setError("Wrong password is entered");
-                            } else if (task.getException().toString().contains("The email")) {
-                                binding.emailINP.setError("Email Does not exist");
-                            }
+                            Toast.makeText(sign_up.this, "Account cannot be created", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
